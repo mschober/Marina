479c0f068b884938756363f350a1fd342a31c80a
Michael Schober <mynetlogins.schober@gmail.com>
2012-04-11 23:11:43 -0700

    omg, length + beam + size object finally works

--- app/models/Slip.java
+++ app/models/Slip.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 
+
 import play.db.jpa.Model;
 
 @Entity
@@ -15,15 +16,33 @@ public class Slip extends Model {
 	@OneToOne
 	public Boat boat;
 
+	@OneToOne
+	public Size size;
+
 	public Slip(String name, Boat boat) {
 		this.name = name;
 		this.boat = boat;
+		this.size = new Size(0, 0);
 	}
 	
 	@Override
 	public String toString(){
 		Serializable formatBoat = (boat == null)? "" : boat;
 		return "[" + name + "]: " + formatBoat;
+	}
+
+	public String toFullString() {
+		return toString().concat("\n").concat(size.toString());
+	}
+
+	public Slip setLength(int length) {
+		this.size.setLength(length).save();
+		return this;
+	}
+
+	public Slip setBeam(int beam) {
+		this.size.setBeam(beam).save();
+		return this;
 	}
 
 }
