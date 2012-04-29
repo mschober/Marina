1dd8bea7a4f54c9375718a781ec69a5cea59bff1
Michael Schober <mynetlogins.schober@gmail.com>
2012-04-28 21:24:21 -0700

    include webdrive testing, use slip.id for the empty textboxes, simplify and cleanup controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,10 +16,6 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-//    	Dock aDock = Dock.find("byName", "A Dock").first();
-//    	Dock bDock = Dock.find("byName", "B Dock").first();
-//    	Dock cDock = Dock.find("byName", "C Dock").first();
-//        render(aDock, bDock, cDock);
         List<Slip> slips = Slip.findAll();
         Arrays.sort(slips.toArray());
         render(slips);
@@ -35,19 +31,9 @@ public class Application extends Controller {
     public static void post(Long slipId, String boatName){
     	Boat createdBoat = new Boat(boatName);
     	createdBoat.save();
-		fillSlip(slipId, createdBoat.id);
-    	index();
-    }
-    
-    public static void fillSlip(Long slipId, Long boatId) {
-    	Boat foundBoat = Boat.findById(boatId);
-    	fillSlip(slipId, foundBoat);
-    	index();
-    }
-
-	private static void fillSlip(Long slipId, Boat foundBoat) {
 		Slip slip = Slip.findById(slipId);
-		slip.boat = foundBoat;
-    	slip.save();
-	}
+		slip.boat = createdBoat;
+		slip.save();
+    	index();
+    }
 }
